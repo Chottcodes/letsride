edd2fc34101dc0b8ef96d6db250dc74d84254d37
Chottcodes <cwellshott@gmail.com>
2025-02-18 15:43:49 -0800

    update to homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,24 @@
-import {
-  Image,
-  StyleSheet,
-  Platform,
-  Text,
-  View,
-  StatusBar,
-} from "react-native";
+import {Image,StyleSheet,Platform,Text,View,StatusBar,} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import {useFonts} from "expo-font";
 
 export default function HomeScreen() {
+  let [fontsLoaded] = useFonts({
+    'Antonio Bold': require('../../assets/fonts/Antonio-Bold.ttf'),
+    'Antonio regular': require('../../assets/fonts/Antonio-Regular.ttf')
+  });
+  const fontFamily = fontsLoaded ? 'Antonio regular' : 'Arial,sans-serif';
+
+
   return (
     <SafeAreaView style={styles.SafeAreaContainer}>
       <StatusBar barStyle="light-content" backgroundColor="black" />
       <View style={styles.viewContainer}>
-        <Image source={require("../../assets/images/LetsRide.png")} style={styles.headerImage} />
-        <Text style={styles.titleContainer}>Nice!!!!!</Text>
+        <Image
+          source={require("../../assets/images/LetsRide.png")}
+          style={styles.headerImage}
+        />
+        <Text style={[styles.text,{fontFamily}]}>Connecting Riders, One Mile At A Time</Text>
       </View>
     </SafeAreaView>
   );
@@ -23,19 +27,23 @@ const styles = StyleSheet.create({
   SafeAreaContainer: {
     flex: 1,
     backgroundColor: "black",
-  },viewContainer: {
-    backgroundColor:"white",
+  },
+  viewContainer: {
     flex: 1,
+    flexDirection: "column",
+    backgroundColor: "black",
+    alignItems: "center",
   },
   headerImage: {
-    width: 300,
-    height: 300,
+    width: 250,
+    height: 250,
     alignSelf: "center",
   },
-  titleContainer: {
-    alignItems: "center",
-    marginTop: 20,
-    color: "black",
-    fontSize: 24,
-  },
+ text:{
+    fontFamily:"Antonio regular",
+    fontSize: 15,
+    textAlign: "center",
+    width:'80%',
+    color:"white",
+  }
 });
